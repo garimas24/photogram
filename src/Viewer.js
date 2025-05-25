a9172ef0b3670b38356309775d0ee4592327d497
--unset <--unset>
2025-05-25 12:24:49 +0530

    third

--- src/Viewer.js
+++ src/Viewer.js
@@ -1,33 +1,109 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+
+// function Viewer({ data, current, setCurrent, exit }) {
+// useEffect(() => {
+// const timer = setTimeout(() => {
+// if (current < data.length - 1) setCurrent(current + 1);
+// else exit();
+// }, 5000);
+
+// return () => clearTimeout(timer);
+// }, [current, data.length, exit, setCurrent]);
+
+// const goBack = () => {
+// if (current > 0) setCurrent(current - 1);
+// };
+
+// const goForward = () => {
+// if (current < data.length - 1) setCurrent(current + 1);
+// else exit();
+// };
+
+
+// return (
+// <div className="overlay">
+// <div className="viewer">
+// <div className="zone left" onClick={goBack} />
+
+// <img src={data[current].url} alt="current-story" className="story-full" />
+// <div className="zone right" onClick={goForward} />
+// <img src='{data}[current}.url'
+// alt='story'
+// className='story-full fade'/>
+// </div>
+// </div>
+
+// );
+// }
+
+// export default Viewer;
+
+
+
+import React, { useEffect, useState, useRef } from 'react';
 
 function Viewer({ data, current, setCurrent, exit }) {
-useEffect(() => {
-const timer = setTimeout(() => {
-if (current < data.length - 1) setCurrent(current + 1);
-else exit();
-}, 5000);
+const [progress, setProgress] = useState(0);
+const intervalRef = useRef(null);
 
-return () => clearTimeout(timer);
-}, [current, data.length, exit, setCurrent]);
+const duration = 5000;
+const updateInterval = 50;
+
+// Function to start the progress bar and auto-advance timer
+const startProgress = () => {
+let timePassed = 0;
+setProgress(0);
+
+intervalRef.current = setInterval(() => {
+timePassed += updateInterval;
+setProgress((timePassed / duration) * 100);
+
+if (timePassed >= duration) {
+clearInterval(intervalRef.current);
+if (current < data.length - 1) {
+setCurrent(current + 1);
+} else {
+exit();
+}
+}
+}, updateInterval);
+};
+
+// Reset timer and progress whenever current story changes
+useEffect(() => {
+clearInterval(intervalRef.current);
+startProgress();
+return () => clearInterval(intervalRef.current);
+}, [current]);
 
 const goBack = () => {
-if (current > 0) setCurrent(current - 1);
+clearInterval(intervalRef.current);
+if (current > 0) {
+setCurrent(current - 1);
+}
 };
 
 const goForward = () => {
-if (current < data.length - 1) setCurrent(current + 1);
-else exit();
+clearInterval(intervalRef.current);
+if (current < data.length - 1) {
+setCurrent(current + 1);
+} else {
+exit();
+}
 };
 
 return (
 <div className="overlay">
 <div className="viewer">
-<div className="zone left" onClick={goBack} />
-<img src={data[current].url} alt="current-story" className="story-full" />
-<div className="zone right" onClick={goForward} />
-<img src='{data}[current}.url'
-alt='story'
-className='story-full fade'/>
+<div className="progress-bar">
+<div
+className="progress-fill"
+style={{ width: `${progress}%` }}
+/>
+</div>
+<div className="zone left" onClick={goBack}></div>
+<img src={data[current].url} alt="story" className="story-full" />
+<div className="zone right" onClick={goForward}></div>
 </div>
 </div>
 );
